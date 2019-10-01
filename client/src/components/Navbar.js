f2b2724ce7c86d904121bc292a15080bbbca9e5f
Marcus Virginia <marcusvirg345@gmail.com>
2019-10-01 14:24:43 -0500

    Added box as a container to the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
+import { Box } from 'grommet'
+
 import styled from 'styled-components'
 
-const NavbarContainer = styled.div`
+const NavbarContainer = styled(Box)`
   height: 50px;
   width: 100%;
   background-color: #0ca7d3;
 `
 
 const Navbar = () => {
-  return <NavbarContainer>Navbar</NavbarContainer>
+  return <NavbarContainer pad="small">Navbar</NavbarContainer>
 }
 
 export default Navbar
