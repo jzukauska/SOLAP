318f335429e258184b3d4cc19f2ef6fc8d981f7e
Peter Wiringa <wiringa@umn.edu>
2019-12-02 11:22:33 -0600

    roll county/tract source components into layer components

--- client/src/components/OpenLayers/MnCountyLayer.js
+++ client/src/components/OpenLayers/MnCountyLayer.js
@@ -2,11 +2,24 @@
  * MN county layer
  */
 import { Vector as VectorLayer } from "ol/layer";
-import MnCountySource from "./MnCountySource";
+import { Vector as VectorSource } from "ol/source";
+import { GeoJSON } from "ol/format";
+import { all as allStrategy } from "ol/loadingstrategy";
 import BasicPolygon from "./Style/BasicPolygon";
 
 export default new VectorLayer({
   name: "mn_county_2010",
-  source: MnCountySource,
+  source: new VectorSource({
+    format: new GeoJSON(),
+    url: function(extent) {
+      return (
+        "http://149.165.157.200:8080/geoserver/ows?service=wfs&" +
+        "version=1.1.0&request=GetFeature&typename=solap:mn_county_2010&" +
+        "outputFormat=application/json&srsname=EPSG:3857"
+      );
+    },
+    attributions: "U.S. Census Bureau",
+    strategy: allStrategy
+  }),
   style: BasicPolygon
 });
--- client/src/components/OpenLayers/MnTractLayer.js
+++ client/src/components/OpenLayers/MnTractLayer.js
@@ -2,12 +2,25 @@
  * MN tract layer
  */
 import { Vector as VectorLayer } from "ol/layer";
-import MnTractSource from "./MnTractSource";
+import { Vector as VectorSource } from "ol/source";
+import { GeoJSON } from "ol/format";
+import { all as allStrategy } from "ol/loadingstrategy";
 import BasicPolygon from "./Style/BasicPolygon";
 
 const layer = new VectorLayer({
   name: "mn_tract_2010",
-  source: MnTractSource,
+  source: new VectorSource({
+    format: new GeoJSON(),
+    url: function(extent) {
+      return (
+        "http://149.165.157.200:8080/geoserver/ows?service=wfs&" +
+        "version=1.1.0&request=GetFeature&typename=solap:mn_tract_2010&" +
+        "outputFormat=application/json&srsname=EPSG:3857"
+      );
+    },
+    attributions: "U.S. Census Bureau",
+    strategy: allStrategy
+  }),
   style: BasicPolygon
 });
 
