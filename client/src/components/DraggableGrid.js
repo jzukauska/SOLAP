c2b7317b295475c51e30624c1d4cc10f4ab4135d
Peter Wiringa <wiringa@umn.edu>
2019-12-04 07:15:15 -0600

    fit app in initial window (still fails to resize)

--- client/src/components/DraggableGrid.js
+++ client/src/components/DraggableGrid.js
@@ -1,45 +1,45 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from "react";
 
-import Muuri from 'muuri'
+import Muuri from "muuri";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const windowHeight = window.innerHeight
+const windowHeight = window.innerHeight;
 
 const DraggableContainer = styled.div.attrs({
-  className: '.draggable-grid'
+  className: ".draggable-grid"
 })`
   position: relative;
   width: 100%;
   background-color: #363636;
-`
+`;
 const DraggableItem = styled.div.attrs({
-  className: 'draggable-grid-item'
+  className: "draggable-grid-item"
 })`
   display: block;
   position: absolute;
-  height: ${windowHeight - 50}px;
+  height: ${windowHeight - 98}px;
   z-index: 1;
   background-color: #dddbe0;
-`
+`;
 
 const DraggableGrid = ({ children }) => {
-  const draggableRef = useRef()
+  const draggableRef = useRef();
   useEffect(() => {
     new Muuri(draggableRef.current, {
-      items: '.draggable-grid-item',
+      items: ".draggable-grid-item",
       dragEnabled: false,
-      dragAxis: 'x',
+      dragAxis: "x",
       dragStartPredicate: (item, e) => {
         // If the item is the map don't allow dragging
         if (item._child.children[0].dataset.nodrag) {
-          return false
+          return false;
         }
         // Return default drag predicate
-        return Muuri.ItemDrag.defaultStartPredicate(item, e)
+        return Muuri.ItemDrag.defaultStartPredicate(item, e);
       }
-    })
-  }, [])
+    });
+  }, []);
   return (
     <DraggableContainer ref={draggableRef}>
       {children.map((child, idx) => {
@@ -52,10 +52,10 @@ const DraggableGrid = ({ children }) => {
               {child}
             </div>
           </DraggableItem>
-        )
+        );
       })}
     </DraggableContainer>
-  )
-}
+  );
+};
 
-export default DraggableGrid
+export default DraggableGrid;
