cbbde2a0a34c8878785bc1baf3f71333eefa22ec
Peter Wiringa <wiringa@umn.edu>
2019-12-09 08:48:21 -0600

    better center on MN

--- client/src/components/OpenLayers/DefaultView.js
+++ client/src/components/OpenLayers/DefaultView.js
@@ -1,10 +1,10 @@
-import View from 'ol/View'
-import { fromLonLat } from 'ol/proj'
+import View from "ol/View";
+import { fromLonLat } from "ol/proj";
 
-const MinneapolisLngLat = [-93.26384, 44.97997]
-const MinneapolisMercator = fromLonLat(MinneapolisLngLat)
+const MinneapolisLngLat = [-93.5, 46];
+const MinneapolisMercator = fromLonLat(MinneapolisLngLat);
 
 export default new View({
   center: MinneapolisMercator,
   zoom: 6
-})
+});
