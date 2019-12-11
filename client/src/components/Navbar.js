8e7636df515662599cde5ad8b6c306ba8680b590
Ananda Poudel <apoudel@stcloudstate.edu>
2019-12-11 15:39:07 -0600

    wip

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useState } from "react";
 import { FilterConsumer } from "./FilterContextProvider";
 import { Box } from "grommet";
-
+import logo from "./che_long.png";
+import Grid from "@material-ui/core/Grid";
 import styled from "styled-components";
 
 const NavbarContainer = styled(Box)`
   height: 50px;
   width: 100%;
-  background-color: #0ca7d3;
+  background-color: #acddde;
 `;
 
 const Navbar = () => {
@@ -16,7 +17,20 @@ const Navbar = () => {
   const [area, setArea] = useState("[Geographic Unit]");
   return (
     <NavbarContainer pad="small">
-      Mapping: {year} for {area}
+      <Grid container>
+        <Grid item xs={6}>
+          <div>
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ height: "30px", verticalAlign: "top" }}
+            />
+          </div>
+        </Grid>
+        <Grid item xs={6}>
+          Mapping: {year} for {area}
+        </Grid>
+      </Grid>
       <FilterConsumer>
         {({
           filterFields,
